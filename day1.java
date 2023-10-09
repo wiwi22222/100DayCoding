a87badb06b1ca91d7f34f6f4c5a0fdea8b40bfaf
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-09 13:39:31 +0800

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,30 @@
+
+package wiwiLestiani;
+
+
+public class day1 {
+    public static void main(String[] args) {
+      String nama = "Wiwi Lestiani" ;
+      int umur = 19 ;
+      long tahunLahir = 2003 ;
+      short tanggalLahir = 10 ;
+      byte bulanLahir = 9 ;
+      double tinggiBadan = 163.0 ;
+      float beratBadan = 51.0f ;
+      char ukuranBaju = 'L' ;
+      boolean statusMenikah = false ;
+      
+        System.out.println("nama :" + nama);
+        System.out.println("umur :" + umur);
+        System.out.println("tahun lahir :" + tahunLahir);
+        System.out.println("tanggal lahir :" + tanggalLahir);
+        System.out.println("bulan lahir :" + bulanLahir);
+        System.out.println("tinggi badan :" + tinggiBadan);
+        System.out.println("berat badan :" + beratBadan);
+        System.out.println("ukuran baju ;" + ukuranBaju);
+        System.out.println("status menikah :" + statusMenikah);
+    }
+    
+}
+    
+
