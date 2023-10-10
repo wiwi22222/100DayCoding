7d51d46e2629164f1d39ac89c9609c08f8d2bcd7
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-10 20:28:57 +0800

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,19 @@
+
+package wiwiLestiani;
+
+public class day2 {
+ 
+    public static void main(String[] args) {
+        
+        int angkaPertama = 5;
+        int angkaKedua = 3;
+        
+        int hasil = angkaPertama + angkaKedua;
+
+        System.out.println("Hasil penambahan: " + hasil);
+    }
+}
+    
+
+
+
