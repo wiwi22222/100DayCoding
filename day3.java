0dd6a6a46198b419ea70b85c6a0c0f283ca9374a
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-11 18:49:36 +0800

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,14 @@
+
+package wiwiLestiani;
+
+public class day3 {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+
+        // Operator "sama dengan" (==)
+        boolean hasilSamaDengan = angka1 == angka2;
+        System.out.println("Apakah angka1 sama dengan angka2? " + hasilSamaDengan);
+    
+    }
+}
