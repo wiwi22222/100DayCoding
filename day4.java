aec7d367041becc0499a03096c7dfc48ff1f4c7d
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-12 21:47:10 +0800

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,18 @@
+
+package wiwiLestiani;
+
+
+public class day4 {
+    
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+
+        boolean hasilTidakSamaDengan = angka1 != angka2;
+
+        System.out.println("Apakah angka1 tidak sama dengan angka2? " + hasilTidakSamaDengan);
+    }
+}
+
+    
+
