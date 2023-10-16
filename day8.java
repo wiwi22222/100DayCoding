ce75c047db3748fbe45bf3090414f9ca23d8b60b
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-16 20:43:37 +0800

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,16 @@
+
+package wiwiLestiani;
+
+public class day8 {
+ 
+    public static void main(String[] args) {
+        int[] angka = {5, 10, 15, 20, 25};
+        System.out.println("Isi array angka:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Angka ke-" + i + ": " + angka[i]);
+        }
+    }
+}
+  
+    
+
