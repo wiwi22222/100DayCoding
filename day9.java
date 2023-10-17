feebf2ecc2bddcd11e6a4ad4ed0ae512426ebc1c
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-17 20:23:56 +0800

    Add files via upload

---
+++ day9.java
@@ -0,0 +1,12 @@
+
+package wiwiLestiani;
+
+public class day9 {
+     public static void main(String[] args) {
+        int angka1 = 23;
+        int angka2 = 7;
+        boolean nilai = angka1 <= angka2;
+        System.out.println("Apakah angka1 kurang dari atau sama dengan angka2? " + nilai);
+    
+    }
+}
