9730c479d865fcd6e11df193a3d5d5c8dcecefec
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-19 21:43:29 +0800

    Add files via upload

---
+++ day11.java
@@ -0,0 +1,16 @@
+
+package wiwiLestiani;
+
+public class day11 {
+    public static void main(String[] args) {
+        
+        String[] nama = {"wiwi", "putri", "dipa", "mute", "naya"};
+
+        System.out.println("Daftar Nama:");
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println(nama[i]);
+        }
+    }
+}
+
+
