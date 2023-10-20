7b91d89a970d8f3216c544090a533fe34a6ad98f
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-20 19:43:51 +0800

    Add files via upload

---
+++ day12.java
@@ -0,0 +1,14 @@
+
+package wiwiLestiani;
+
+public class day12 {
+    public static void main(String[] args) {
+        int angka1 = 17;
+        int angka2 = 5;
+        int sisa = angka1 % angka2;
+        System.out.println("Sisa pembagian: " + sisa);
+    }
+}
+
+    
+
