fb6d473a7e6457812dcaf462bd2df03e11db6a37
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-21 20:22:41 +0800

    Add files via upload

---
+++ day13.java
@@ -0,0 +1,18 @@
+
+package wiwiLestiani;
+
+public class day13 {
+   
+    public static void main(String[] args) {
+        int angka = -5;
+
+        if (angka > 0) {
+            System.out.println("Angka adalah positif.");
+        } else {
+            System.out.println("Angka adalah negatif atau nol.");
+        }
+    }
+}
+
+    
+
