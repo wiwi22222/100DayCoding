cae4f98893c265e9af4d95e09662f69ff14136ed
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-22 17:27:55 +0800

    Add files via upload

---
+++ day14.java
@@ -0,0 +1,23 @@
+
+package wiwiLestiani;
+
+public class day14 {
+    public static void main(String[] args) {
+        //int nilai = 75;
+
+        if (nilai >= 90) {
+            System.out.println("Nilai : A");
+        } else if (nilai >= 80) {
+            System.out.println("Nilai : B");
+        } else if (nilai >= 70) {
+            System.out.println("Nilai : C");
+        } else if (nilai >= 60) {
+            System.out.println("Nilai : D");
+        } else {
+            System.out.println("Nilai : E");
+        }
+    }
+}
+
+    
+
