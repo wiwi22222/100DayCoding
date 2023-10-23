bf8625537fdb9994f0cce6cfb34a399c5071afce
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-23 15:24:19 +0800

    Add files via upload

---
+++ day15.java
@@ -0,0 +1,17 @@
+
+package wiwiLestiani;
+
+public class day15 {
+    public static void main(String[] args) {
+        int usia = 7;
+
+        if (usia < 12) {
+            System.out.println("Anak-anak");
+        } else if (usia < 18) {
+            System.out.println("Remaja");
+        } else {
+            System.out.println("Dewasa");
+        }
+    }
+}
+
