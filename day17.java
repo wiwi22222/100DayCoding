8dce80a076a04f4b28cb1998e7a37cc1f7270b1e
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-25 19:08:17 +0800

    Add files via upload

---
+++ day17.java
@@ -0,0 +1,14 @@
+
+package wiwiLestiani;
+
+public class day17 {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 5) {
+      System.out.println(i);
+      i++;
+    }  
+  }
+}
+
+   
