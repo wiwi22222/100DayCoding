1edbd35f0f3ffb5b90f28fe2977b2e20d351e533
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-26 21:55:59 +0800

    Add files via upload

---
+++ day18.java
@@ -0,0 +1,12 @@
+
+package wiwiLestiani;
+
+public class day18 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+        for (int nilai : angka) {
+            System.out.println(nilai);
+        }
+    }
+}
+
