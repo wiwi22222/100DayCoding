8ba8511d6295c4146dc4338c8e44f73d2e07b205
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-27 20:06:53 +0800

    Add files via upload

---
+++ day19.java
@@ -0,0 +1,14 @@
+
+package wiwiLestiani;
+
+public class day19 {
+    public static void main(String[] args) {
+        double jariJari = 3.0;
+        double luas = Math.PI * jariJari * jariJari;
+        System.out.println("Luas lingkaran: " + luas);
+    }
+}
+
+ 
+
+    
