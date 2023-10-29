a2c5e7ab7a4dcb535ccf1a83665b5e815e36a8b9
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-10-29 19:53:36 +0800

    Add files via upload

---
+++ day21.java
@@ -0,0 +1,13 @@
+
+package wiwiLestiani;
+
+ import java.util.Date;
+
+public class day21 {
+    public static void main(String[] args) {
+        Date tanggalSaatIni = new Date();
+        System.out.println("Tanggal dan waktu saat ini: " + tanggalSaatIni);
+    }
+}
+
+
