5232b4032dc75c4ffbab33386f9c051b1e149086
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-11-02 20:12:49 +0800

    Add files via upload

---
+++ day25.java
@@ -0,0 +1,15 @@
+
+package wiwiLestiani;
+
+public class day25 {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("Operasi AND: " + (benar && salah));
+        System.out.println("Operasi OR: " + (benar || salah));
+        System.out.println("Operasi NOT: " + (!benar));
+    }
+}
+
+    
