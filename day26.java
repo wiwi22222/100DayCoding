bb1722018d24dbf34ab76fa0779205e737e5889b
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-11-03 18:48:08 +0800

    Add files via upload

---
+++ day26.java
@@ -0,0 +1,18 @@
+
+package wiwiLestiani;
+
+public class day26 {
+    public static void main(String[] args) {
+        int n = 29;
+        int total = 0;
+
+        for (int i = 1; i <= n; i++) {
+            total += i;
+        }
+
+        System.out.println("Total penjumlahan: " + total);
+    }
+}
+
+    
+
