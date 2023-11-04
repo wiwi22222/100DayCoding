3a1e7f683bc0c90c2cc422926e5b1831ffe8c049
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-11-04 21:41:32 +0800

    Add files via upload

---
+++ day27.java
@@ -0,0 +1,11 @@
+
+package wiwiLestiani;
+
+public class day27 {
+    public static void main(String[] args) {
+        int sisi = 40; 
+        int volume = sisi * sisi * sisi;
+
+        System.out.println("Volume kubus adalah: " + volume);
+    }    
+}
