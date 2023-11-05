52a8eb4720d16faea25424fe6e0ebe5841f5bed7
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-11-05 19:58:18 +0800

    Add files via upload

---
+++ day28.java
@@ -0,0 +1,12 @@
+
+package wiwiLestiani;
+
+public class day28 {
+    public static void main(String[] args) {
+        int angka = 3;
+        int pangkat = 4;
+        int hasil = (int) Math.pow(angka, pangkat);
+        System.out.println(angka + " pangkat " + pangkat + " = " + hasil);
+    }
+}
+
