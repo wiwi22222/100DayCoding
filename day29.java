381826a62976503bc969f974428a00be897a3363
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-11-06 19:15:47 +0800

    Add files via upload

---
+++ day29.java
@@ -0,0 +1,17 @@
+
+package wiwiLestiani;
+
+public class day29 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+        
+        System.out.println("Isi array angka:");
+        
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Angka ke-" + i + ": " + angka[i]);
+        }
+    }
+}
+    
+    
+
