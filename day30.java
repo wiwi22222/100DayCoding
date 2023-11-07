f4ef4e4e55cee9c5b1a37f85910bc49ac5706998
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-11-07 22:21:49 +0800

    Add files via upload

---
+++ day30.java
@@ -0,0 +1,23 @@
+
+package wiwiLestiani;
+
+import java.util.Vector;
+
+public class day30 {
+     public static void main(String[] args) {
+        
+        Vector<String> vector = new Vector<>();
+
+        vector.add("haii wiwi");
+        vector.add("ini");
+        vector.add("adalah");
+        vector.add("contoh");
+        vector.add("vektor");
+
+        for (String element : vector) {
+            System.out.println(element);
+        }
+    }
+}
+   
+
