008d5c3c9d957c98415cf94a1d2b2c3de0edf8e5
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2023-11-16 20:25:37 +0800

    Add files via upload

---
+++ day39.java
@@ -0,0 +1,18 @@
+
+package wiwiLestiani;
+
+public class day39 {
+  public static void main(String[] args) {
+    // Outer loop.
+    for (int i = 1; i <= 2; i++) {
+      System.out.println("box: " + i);
+
+      // Inner loop.
+      for (int j = 1; j <= 3; j++) {
+        System.out.println("  gelas: " + j);
+      }
+    }
+  }
+}
+
+    
