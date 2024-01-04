f5326ff0b67c9857ca68a2d922b660cd8994441e
wiwi22222 <100199703+wiwi22222@users.noreply.github.com>
2024-01-04 12:41:07 +0800

    Add files via upload

---
+++ day88.java
@@ -0,0 +1,45 @@
+
+package wiwiLestiani;
+
+import java.util.Scanner;
+        
+public class day88 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan pola yang diinginkan : \n1. segitiga\n2. persegi\n: ");
+        int pola = sc.nextInt();
+            if(pola>2){
+                System.out.println("pola hanya tersedia untuk persegi dan segitiga");
+            }   
+
+        System.out.print("tinggi pola: ");
+        int tinggi = sc.nextInt();
+
+        hasilpola(pola, tinggi);
+
+        sc.close();
+    }
+
+public static void hasilpola(int pola, int tinggi) {
+        if (pola==1) {
+            for (int i = 1; i <= tinggi; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } else if (pola==2) {
+            for (int i = 0; i < tinggi; i++) {
+                for (int j = 0; j < tinggi; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("hanya tersedia pola segitiga dan persegi");
+        }      
+    }
+    
+}
+    
